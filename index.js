648bd9b662da829b604564023d51ab00425c9e51
Ace <acekiron@outlook.com>
2023-07-24 21:28:38 +0200

    Next test

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 const io = require("@actions/io");
+const artifact = require("@actions/artifact");
 
 const path = require("path");
 
-(async function() {
-    const version = core.getInput("minecraft-version");
-    const buildtools = core.getInput("buildtools");
+const artifactClient = artifact.create();
 
-    if (core.isDebug())
+const version = core.getInput("minecraft-version");
+const buildtools = core.getInput("buildtools");
+const artifactName = core.getInput("artifact-name");
+
+(async function() {
+    if (core.isDebug()) {
         await exec.exec(`curl -v -o BuildTools.jar ${buildtools}`);
-    else
+        await exec.exec(`curl -v -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
+    } else {
         await exec.exec(`curl -o BuildTools.jar ${buildtools}`);
+        await exec.exec(`curl -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
+    }
 
     await exec.exec(`java -jar BuildTools.jar --rev ${version}`);
-
-    await io.mv(path.join(__dirname, "Spigot", "Spigot-Server", "target", "spigot-*.jar"), path.join(__dirname, "server.jar"));
     
-    await exec.exec(`java -jar server.jar`);
+    await io.mkdirP(path.join(__dirname, "plugins"));
+
+    const downloadResponse = await artifactClient.downloadArtifact(artifactName, path.join(__dirname, "plugins"), {
+        createArtifactFolder: false
+    });
+
+    await exec.exec(`java -jar spigot-${version}.jar`);
 })().catch(err => {
     core.setFailed(`Failed to test plugin: ${err}`);
 });
