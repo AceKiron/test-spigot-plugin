2534dfb8edacc4a7ed114ab3fd9d03f78160ada9
Ace <acekiron@outlook.com>
2023-07-24 21:19:25 +0200

    Next test

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const io = require("@actions/io");
+
+const path = require("path");
 
 (async function() {
     const version = core.getInput("minecraft-version");
     const buildtools = core.getInput("buildtools");
 
-    await exec.exec(`curl -o BuildTools.jar ${buildtools}`);
+    if (core.isDebug())
+        await exec.exec(`curl -v -o BuildTools.jar ${buildtools}`);
+    else
+        await exec.exec(`curl -o BuildTools.jar ${buildtools}`);
+
+    await exec.exec(`java -jar BuildTools.jar --rev ${version}`);
+
+    await io.mv(path.join(__dirname, "Spigot", "Spigot-Server", "target", "spigot-*.jar"), path.join(__dirname, "server.jar"));
+    
+    await exec.exec(`java -jar server.jar`);
 })().catch(err => {
     core.setFailed(`Failed to test plugin: ${err}`);
 });
