c11c09a1f72ef1759770934445d197b16758625d
Ace <acekiron@outlook.com>
2023-07-25 08:42:46 +0200

    Dynamic Java version selection

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ const buildtools = core.getInput("buildtools");
 const artifactName = core.getInput("artifact-name");
 
 (async function() {
-    await exec.exec("sudo apt install java-common openjdk-17-jdk openjdk-17-jre");
-    await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64");
+    switch (version) {
+        case "1.13.2": case "1.14.4": case "1.15.2": case "1.16.5":
+            await exec.exec("sudo add-apt-repository ppa:linuxuprising/java");
+            await exec.exec("sudo apt update");
+            await exec.exec("sudo apt install oracle-java16-installer");
+            await exec.exec("sudo update-java-alternatives --list");
+            break;
+
+        case "1.17.1": case "1.18.2": case "1.19.4": case "1.20.1":
+            await exec.exec("sudo apt install java-common openjdk-17-jdk openjdk-17-jre");
+            await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64");
+            break;
+        
+        default:
+            core.setFailed(`Unsupported Minecraft version: ${version}`);
+            return;
+    }
 
     if (core.isDebug()) {
         await exec.exec(`curl -v -o BuildTools.jar ${buildtools}`);
