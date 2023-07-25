b6ebb98b4150794d041156b3f38dc5a26074f013
Ace <acekiron@outlook.com>
2023-07-25 10:15:55 +0200

    Log all required messages

--- index.js
+++ index.js
@@ -13,62 +13,78 @@ const version = core.getInput("minecraft-version");
 const buildtools = core.getInput("buildtools");
 const artifactName = core.getInput("artifact-name");
 
-const yamlFile = yaml.parse(fs.readFileSync(path.join(".", ".acekiron", "test-spigot-plugin.yml")));
-console.log(yamlFile);
-// console.log(yaml.parse(fs.readFileSync(path.join(".", ".acekiron", "test-spigot-plugin.yml")))["required-messages"]);
+const requiredMessages = yaml.parse(fs.readFileSync(path.join(".", ".acekiron", "test-spigot-plugin.yml"), "utf8"))["required-messages"];
 
-// (async function() {
+class Test {
+    constructor() {
+        this.requiredMessagesLeft = requiredMessages;
+    }
 
-    // console.log(yaml.parse(fs.readFileSync(path.join(".", ".acekiron", "test-spigot-plugin.yml")))["required-messages"]);
+    isSuccess() {
+        if (this.requiredMessagesLeft.length > 0) return false;
+        return true;
+    }
+}
+const test = new Test();
 
-    // switch (version) {
-    //     case "1.13.2": case "1.14.4": case "1.15.2": case "1.16.5":
-    //         await exec.exec("sudo apt install java-common openjdk-8-jdk openjdk-8-jre");
-    //         await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.8.0-openjdk-amd64");
+(async function() {
+    switch (version) {
+        case "1.13.2": case "1.14.4": case "1.15.2": case "1.16.5":
+            await exec.exec("sudo apt install java-common openjdk-8-jdk openjdk-8-jre");
+            await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.8.0-openjdk-amd64");
             
-    //         break;
+            break;
 
-    //     case "1.17.1": case "1.18.2": case "1.19.4": case "1.20.1":
-    //         await exec.exec("sudo apt install java-common openjdk-17-jdk openjdk-17-jre");
-    //         await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64");
+        case "1.17.1": case "1.18.2": case "1.19.4": case "1.20.1":
+            await exec.exec("sudo apt install java-common openjdk-17-jdk openjdk-17-jre");
+            await exec.exec("sudo update-java-alternatives --set /usr/lib/jvm/java-1.17.0-openjdk-amd64");
             
-    //         break;
+            break;
         
-    //     default:
-    //         core.setFailed(`Unsupported Minecraft version: ${version}`);
+        default:
+            core.setFailed(`Unsupported Minecraft version: ${version}`);
             
-    //         return;
-    // }
+            return;
+    }
 
-    // if (core.isDebug()) {
-    //     await exec.exec(`curl -v -o BuildTools.jar ${buildtools}`);
-    //     await exec.exec(`curl -v -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
-    // } else {
-    //     await exec.exec(`curl -o BuildTools.jar ${buildtools}`);
-    //     await exec.exec(`curl -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
-    // }
+    if (core.isDebug()) {
+        await exec.exec(`curl -v -o BuildTools.jar ${buildtools}`);
+        await exec.exec(`curl -v -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
+    } else {
+        await exec.exec(`curl -o BuildTools.jar ${buildtools}`);
+        await exec.exec(`curl -o eula.txt https://raw.githubusercontent.com/AceKiron/test-spigot-plugin/main/accept-eula.txt`);
+    }
 
-    // await exec.exec(`java -jar BuildTools.jar --rev ${version} --compile spigot`);
+    await exec.exec(`java -jar BuildTools.jar --rev ${version} --compile spigot`);
     
-    // await io.mkdirP(path.join(__dirname, "plugins"));
-    // await artifactClient.downloadArtifact(artifactName, path.join(__dirname, "plugins"), {
-    //     createArtifactFolder: false
-    // });
+    await io.mkdirP(path.join(__dirname, "plugins"));
+    await artifactClient.downloadArtifact(artifactName, path.join(__dirname, "plugins"), {
+        createArtifactFolder: false
+    });
 
-    // const promise = exec.exec(`java -jar spigot-${version}.jar`, undefined, {
-    //     listeners: {
-    //         stdout: (data) => {
-    //             if (data.toString().match(/^\[.+WARN\].+/)) console.warn(data.toString());
-    //             else if (data.toString().match(/^\[.+ERROR\].+/)) console.error(data.toString());
+    const promise = exec.exec(`java -jar spigot-${version}.jar`, undefined, {
+        listeners: {
+            stdout: (data) => {
+                if (data.toString().match(/^\[.+WARN\].+/)) console.warn(data.toString());
+                else if (data.toString().match(/^\[.+ERROR\].+/)) console.error(data.toString());
 
-    //             else if (data.toString().match(/^\[.+INFO\]: Done \(.+s\)! For help, type "help"\n/g) != null) console.log("finish");
-    //         },
-    //         stderr: (data) => {
-    //             console.error(data.toString());
-    //         }
-    //     },
-    //     input: "stop"
-    // });
-// })().catch(err => {
-//     core.setFailed(`Failed to test plugin: ${err}`);
-// });
+                else if (data.toString().match(/^\[.+INFO\]: Done \(.+s\)! For help, type "help"\n/g) != null) console.log("finish");
+
+                else {
+                    for (const message of test.requiredMessagesLeft) {
+                        if (data.toString().match(new RegExp(`^\[.+INFO\]: ${message}`)) != null) {
+                            delete test.requiredMessagesLeft[test.requiredMessagesLeft.indexOf(message)];
+                            break;
+                        }
+                    }
+                }
+            },
+            stderr: (data) => {
+                console.error(data.toString());
+            }
+        },
+        input: "stop"
+    });
+})().catch(err => {
+    core.setFailed(`Failed to test plugin: ${err}`);
+});
